d19724db32def30532dbee2abcfaba2e95835c98
enzomarin <enzomarin12@gmail.com>
2025-05-01 12:59:37 -0400

    remove key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,8 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const supabaseUrl = "https://eevlozkntiedveaytdjm.supabase.co";
-const supabasesKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVldmxvemtudGllZHZlYXl0ZGptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MzMyMDksImV4cCI6MjA2MTEwOTIwOX0.CkL4cw9iNF_JkSJQ_dxqoB1Z9VABvNuqk_69kE4Doas";
+const supabasesKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "";
 
 export const supabase = createClient(supabaseUrl, supabasesKey, {
   auth: {
